ee22ced4fcf9a2383c919ffa9b6a6919b93f3b28
Dan Kantor <dan@ex.fm>
2012-09-06 12:47:58 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# progress-bar
+
+
+
+
+## Install
+
+
+     npm install progress-bar
+
+## Testing
+
+    git clone 
+    npm install
+    open test/index.html
---
+++ test/progress-bar.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("progress-bar", function(){
+    it("should have at least one test", function(){
+        assert.fail("Write a test!");
+    });
+});
